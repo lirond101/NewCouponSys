aed584f892b9de8f28cc79fb2c034f388870d4c7
lirond101 <lirond101@gmail.com>
2015-04-29 19:39:56 +0300

    unit tests added

--- UnitTestProject2/UnitTest1.cs
+++ UnitTestProject2/UnitTest1.cs
@@ -9,6 +9,9 @@ namespace UnitTestProject2
     {
         Coupon coupon;
         Deal deal;
+        Customer customer;
+        Store store;
+        City city;
 
         [TestMethod]
         public void TestCoupon()
@@ -37,11 +40,60 @@ namespace UnitTestProject2
             {
                 deal = new Deal();
                 deal.dealCode = "123ewq";
-                deal.Customer_user_name = "eden";
+                //deal.Customer_user_name = "eden";
                 deal.expiration_date = DateTime.Today;
                 deal.order_date = DateTime.Today;
 
                 db.s_Deals.Add(deal);
+                db.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void TestCustomer()
+        {
+            using (var db = new CouponContext())
+            {
+                customer = new Customer();
+                customer.email = "eden101@gmail.com";
+                customer.user_name = "eden101";
+                customer.first_name = "eden";
+                customer.last_name = "yaakobi";
+                customer.password = "ey123";
+                customer.phone = "0528757474";
+                customer.date_of_birth = DateTime.Now;
+
+                db.s_Customers.Add(customer);
+                db.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void TestStore()
+        {
+            using (var db = new CouponContext())
+            {
+                store = new Store();
+                store.name = "safari";
+                store.description = "the biggest zoo in meadle east";
+                store.phone_num = 4300;
+                store.rank = 7;
+                //store.so.user_name = "zoomer";
+
+                db.s_Stores.Add(store);
+                db.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void TestCity()
+        {
+            using (var db = new CouponContext())
+            {
+                city = new City();
+                city.name = "Tel Aviv";
+
+                db.s_Cities.Add(city);
                 db.SaveChanges();
             }
         }
